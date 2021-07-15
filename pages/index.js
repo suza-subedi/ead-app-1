fb7e7451f93fa9b7d1efee92f78983f85d3feeda
Chayapol Moemeng <mchayapol@scitech.au.edu>
2021-07-15 16:28:15 +0700

    Fixed greeting line

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          This is my first Next.js app.
         </p>
 
         <div className={styles.grid}>
